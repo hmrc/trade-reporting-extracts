1a6b07630db5eeca4c7060ff951950b1878304f0
mounkumar <49392940+Mounkumar@users.noreply.github.com>
2025-02-26 11:17:47 +0000

    modified CompanyInformationController to make use of Future

--- app/uk/gov/hmrc/tradereportingextracts/controllers/CompanyInformationController.scala
+++ app/uk/gov/hmrc/tradereportingextracts/controllers/CompanyInformationController.scala
@@ -30,7 +30,9 @@ class CompanyInformationController @Inject()(customsDataStoreConnector: CustomsD
   extends BackendController(cc):
 
   def companyInformation(eori: String): Action[AnyContent] = Action.async { implicit request =>
-    customsDataStoreConnector.getCompanyInformation(eori).map(companyInformation => {
-      Ok(Json.toJson(companyInformation))
-    })
+    Future {
+      customsDataStoreConnector.getCompanyInformation(eori).map { companyInformation =>
+        Ok(Json.toJson(companyInformation))
+      }
+    }.flatten
   }
