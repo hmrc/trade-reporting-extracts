a20301352bc15cd2738bb0ebde48f8c1e03f8df0
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2025-04-01 11:47:14 +0100

    TRE-326: Add ReportRequestService and its tests for report management… (#17)
    * TRE-326: Add ReportRequestService and its tests for report management functionality
    
    * TRE-326: Fix formatting in ReportRequestService.scala by adding a space in the comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,20 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-
   private val bootstrapVersion = "9.11.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatestplus" %% "mockito-4-11" % "3.2.18.0",
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
-    )
+    "org.scalatestplus" %% "mockito-4-11"            % "3.2.18.0",
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test
+  )
 
   val it: Seq[ModuleID] = Seq.empty
 }
