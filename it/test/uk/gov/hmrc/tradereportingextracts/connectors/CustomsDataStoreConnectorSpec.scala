4532177d0f122c43a66f9bb3c9a06acb321d0eaf
MichaelAAbbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-05-21 15:59:11 +0100

    Adjusted URLs

--- it/test/uk/gov/hmrc/tradereportingextracts/connectors/CustomsDataStoreConnectorSpec.scala
+++ it/test/uk/gov/hmrc/tradereportingextracts/connectors/CustomsDataStoreConnectorSpec.scala
@@ -1,21 +1,36 @@
 package uk.gov.hmrc.tradereportingextracts.connectors
 
 import com.github.tomakehurst.wiremock.client.WireMock
-import uk.gov.hmrc.tradereportingextracts.utils.{SpecBase, WireMockHelper}
-import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, ok, post, urlEqualTo}
+import uk.gov.hmrc.tradereportingextracts.utils.WireMockHelper
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpec
-import org.scalatest.matchers.must.Matchers.{a, mustBe}
+import org.scalatest.matchers.must.Matchers.mustBe
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.test.Helpers.running
 import play.api.{Application, inject}
 import uk.gov.hmrc.http.UpstreamErrorResponse
+import uk.gov.hmrc.tradereportingextracts.config.AppConfig
 import uk.gov.hmrc.tradereportingextracts.models.NotificationEmail
+import org.scalatestplus.play.*
+import play.api.test.*
+import play.api.test.Helpers.*
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
+import java.net.{URI, URL}
 import java.time.LocalDateTime
 
 class CustomsDataStoreConnectorSpec extends AnyFreeSpec with ScalaFutures
+  with GuiceOneAppPerSuite
+  with MockitoSugar
+  with Matchers
   with WireMockHelper {
+
+  val appConfig: AppConfig = app.injector.instanceOf[AppConfig]
+  val baseUrlCDS: String = appConfig.customsDataStore
+  val uri = new URI(baseUrlCDS)
+  val path = uri.getPath
 
   private def application: Application =
     new GuiceApplicationBuilder()
@@ -23,6 +38,8 @@ class CustomsDataStoreConnectorSpec extends AnyFreeSpec with ScalaFutures
       .build()
 
   "getVerifiedEmailForReport" - {
+
+    val url = path ++ "/eori/verified-email"
 
     "Must return verified email when OK" in {
 
@@ -36,7 +53,7 @@ class CustomsDataStoreConnectorSpec extends AnyFreeSpec with ScalaFutures
       running(app) {
         val connector = app.injector.instanceOf[CustomsDataStoreConnector]
         server.stubFor(
-          WireMock.get(urlEqualTo("/trade-reporting-extracts-stub/eori/verified-email"))
+          WireMock.get(urlEqualTo(url))
             .willReturn(ok(responseBody))
         )
 
@@ -49,12 +66,11 @@ class CustomsDataStoreConnectorSpec extends AnyFreeSpec with ScalaFutures
     }
 
     "Must return upstream error response when not OK" in {
-
       val app = application
       running(app) {
         val connector = app.injector.instanceOf[CustomsDataStoreConnector]
         server.stubFor(
-          WireMock.get(urlEqualTo("/trade-reporting-extracts-stub/eori/verified-email"))
+          WireMock.get(urlEqualTo(url))
             .willReturn(aResponse.withStatus(500))
         )
 
