d21bcbd3053fb0f7232bbe77edd90a6a2103c0b4
GaneshMaddipoti <maddipoti.ganesh@gmail.com>
2025-05-21 12:01:30 +0100

    Tre 178 (#24)
    * TRE-178: Add SDES metadata models and ReportAvailabilityNotificationController
    
    * TRE-178: Add notifyReportAvailable endpoint and refactor ReportAvailabilityNotificationController
    
    * TRE-178: Refactor ReportAvailablePayloadMetadataInner to use sealed trait and add metadata item cases
    
    * TRE-178: Add OAS specifications for Trader Report Request and Status Notification APIs
    
    * TRE-178: Implement EisReportStatus API with request and response models, and add header validation
    
    * TRE-178: Add Update EORI API specification and related models for SDES integration
    
    * TRE-178: Refactor FileAvailableMetadataItem to use 'metadata' instead of 'key' and update related JSON reads/writes
    
    * TRE-178: Rename ReportAvailablePayload and related components to FileNotification for clarity, update routes and JSON handling accordingly
    
    * TRE-178: Update SDES authentication token configuration to use hyphenated key format
    
    * TRE-178: Fix authorization header handling in FileNotificationController and add unit tests for controller actions
    
    * TRE-178: Add TRE-ALL collection with API requests for company information, EORI history, verified email, report requests, file notifications, user information, and files list
    
    * TRE-178: Add serverOtherMethods to FileNotificationController and update routes to handle additional HTTP methods
    
    * TRE-178: Formatted

---
+++ app/uk/gov/hmrc/tradereportingextracts/models/sdes/FileNotificationMetadata.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradereportingextracts.models.sdes
+
+import play.api.libs.json._
+
+sealed trait FileNotificationMetadata {
+  def key: String
+  def value: String
+}
+
+object FileNotificationMetadata {
+  case class RetentionDaysMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "RETENTION_DAYS"
+  }
+  case class FileTypeMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "FILE_TYPE"
+  }
+  case class EORIMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "EORI"
+  }
+  case class MDTPReportXCorrelationIDMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "MDTP-report-x-correlationID"
+  }
+  case class MDTPReportRequestIDMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "MDTP-report-requestID"
+  }
+  case class MDTPReportTypeNameMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "MDTP-reportTypeName"
+  }
+  case class ReportFilesPartsMetadataItem(value: String) extends FileNotificationMetadata {
+    val key = "Report-files-parts"
+  }
+
+  // JSON Reads/Writes
+  implicit val reads: Reads[FileNotificationMetadata] = Reads { json =>
+    (json \ "key").validate[String].flatMap {
+      case "RETENTION_DAYS"              => (json \ "value").validate[String].map(RetentionDaysMetadataItem(_))
+      case "FILE_TYPE"                   => (json \ "value").validate[String].map(FileTypeMetadataItem(_))
+      case "EORI"                        => (json \ "value").validate[String].map(EORIMetadataItem(_))
+      case "MDTP-report-x-correlationID" =>
+        (json \ "value").validate[String].map(MDTPReportXCorrelationIDMetadataItem(_))
+      case "MDTP-report-requestID"       => (json \ "value").validate[String].map(MDTPReportRequestIDMetadataItem(_))
+      case "MDTP-reportTypeName"         => (json \ "value").validate[String].map(MDTPReportTypeNameMetadataItem(_))
+      case "Report-files-parts"          => (json \ "value").validate[String].map(ReportFilesPartsMetadataItem(_))
+      case other                         => JsError(s"Unknown metadata key: $other")
+    }
+  }
+
+  implicit val writes: Writes[FileNotificationMetadata] = Writes {
+    case RetentionDaysMetadataItem(value)            => Json.obj("key" -> "RETENTION_DAYS", "value" -> value)
+    case FileTypeMetadataItem(value)                 => Json.obj("key" -> "FILE_TYPE", "value" -> value)
+    case EORIMetadataItem(value)                     => Json.obj("key" -> "EORI", "value" -> value)
+    case MDTPReportXCorrelationIDMetadataItem(value) =>
+      Json.obj("key" -> "MDTP-report-x-correlationID", "value" -> value)
+    case MDTPReportRequestIDMetadataItem(value)      => Json.obj("key" -> "MDTP-report-requestID", "value" -> value)
+    case MDTPReportTypeNameMetadataItem(value)       => Json.obj("key" -> "MDTP-reportTypeName", "value" -> value)
+    case ReportFilesPartsMetadataItem(value)         => Json.obj("key" -> "Report-files-parts", "value" -> value)
+  }
+
+  implicit val format: Format[FileNotificationMetadata] = Format(reads, writes)
+}
---
+++ test/uk/gov/hmrc/tradereportingextracts/controllers/FileNotificationControllerSpec.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradereportingextracts.controllers
+
+import play.api.Application
+import play.api.libs.json.Json
+import play.api.test.Helpers.*
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.tradereportingextracts.models.sdes.*
+import uk.gov.hmrc.tradereportingextracts.utils.SpecBase
+
+class FileNotificationControllerSpec extends SpecBase {
+
+  "FileNotificationController" should {
+    "return 400 BadRequest" in new Setup {
+      val request = FakeRequest(PUT, routes.FileNotificationController.fileNotification().url)
+
+      val result = route(app, request).value
+      status(result) shouldBe BAD_REQUEST
+    }
+  }
+  "FileNotificationController" should {
+    "return 403 Forbidden" in new Setup {
+      val request = FakeRequest(PUT, routes.FileNotificationController.fileNotification().url)
+        .withHeaders(
+          "content-type"          -> "application/json",
+          "authorization"         -> "Invalid-auth-token",
+          "date"                  -> "Mon, 02 Oct 2023 14:30:00 GMT",
+          "x-correlation-id"      -> "asfd-asdf-asdf",
+          "source-system"         -> "SDES",
+          "x-transmitting-system" -> "SDES"
+        )
+
+      val result = route(app, request).value
+      println(contentAsString(result))
+      status(result) shouldBe FORBIDDEN
+    }
+  }
+  "FileNotificationController" should {
+    "return 201 Created" in new Setup {
+      val fileNotification = FileNotification(
+        eori = "GB123456789012",
+        fileName = "testFileName",
+        fileSize = 12345,
+        metadata = List(
+          FileNotificationMetadata.RetentionDaysMetadataItem("30"),
+          FileNotificationMetadata.FileTypeMetadataItem("CSV"),
+          FileNotificationMetadata.EORIMetadataItem("GB123456789012"),
+          FileNotificationMetadata.MDTPReportXCorrelationIDMetadataItem("asfd-asdf-asdf"),
+          FileNotificationMetadata.MDTPReportRequestIDMetadataItem("TRE-19"),
+          FileNotificationMetadata.MDTPReportTypeNameMetadataItem("IMPORT-HEADER"),
+          FileNotificationMetadata.ReportFilesPartsMetadataItem("1of2")
+        )
+      )
+      val request          = FakeRequest(PUT, routes.FileNotificationController.fileNotification().url)
+        .withHeaders(
+          "authorization"         -> "SdesAuthToken",
+          "date"                  -> "Mon, 02 Oct 2023 14:30:00 GMT",
+          "x-correlation-id"      -> "asfd-asdf-asdf",
+          "source-system"         -> "SDES",
+          "x-transmitting-system" -> "SDES"
+        )
+        .withBody(Json.toJson(fileNotification))
+
+      val result = route(app, request).value
+      status(result) shouldBe CREATED
+    }
+  }
+  "FileNotificationController" should {
+    "return 405 MethodNotAllowed" in new Setup {
+      val request = FakeRequest(GET, routes.FileNotificationController.fileNotification().url)
+
+      val result = route(app, request).value
+      status(result) shouldBe METHOD_NOT_ALLOWED
+    }
+  }
+
+  trait Setup {
+    val app: Application = application.build()
+  }
+
+}
