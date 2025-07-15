3e3bdb438c08921986c8e6dbda1f37644238c70a
mounkumar pradhana <49392940+Mounkumar@users.noreply.github.com>
2025-07-15 10:04:21 +0100

    TRE-409 update static email to dynamic email (#81)
    * TRE-409 update static email to dynamic email
    
    * TRE-409 formatting files

--- test/uk/gov/hmrc/tradereportingextracts/controllers/UserControllerSpec.scala
+++ test/uk/gov/hmrc/tradereportingextracts/controllers/UserControllerSpec.scala
@@ -17,51 +17,143 @@
 package uk.gov.hmrc.tradereportingextracts.controllers
 
 import org.mockito.Mockito.when
-import org.scalatest.freespec.AnyFreeSpec
-import org.scalatest.matchers.must.Matchers
-import org.scalatestplus.mockito.MockitoSugar
-import play.api.libs.json.Json
-import play.api.test.*
+import play.api.{Application, inject}
+import play.api.libs.json.{JsArray, JsObject, JsString, Json}
+import play.api.mvc.{AnyContentAsEmpty, Result}
 import play.api.test.Helpers.*
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.tradereportingextracts.models.{CompanyInformation, NotificationEmail, UserDetails}
 import uk.gov.hmrc.tradereportingextracts.services.UserService
+import uk.gov.hmrc.tradereportingextracts.utils.SpecBase
 
+import java.time.LocalDateTime
 import scala.concurrent.Future
 
-class UserControllerSpec extends AnyFreeSpec with Matchers with MockitoSugar {
-  "UserControllerSpec" - {
-    "getAuthorisedEoris" - {
+class UserControllerSpec extends SpecBase {
 
-      val mockUserService = mock[UserService]
-      val cc              = Helpers.stubControllerComponents()
-      val controller      = new UserController(mockUserService, cc)(using cc.executionContext)
+  "UserController.getNotificationEmail" should {
 
-      val testEori        = "EORI1234"
-      val authorisedEoris = Seq("AUTH-EORI-1", "AUTH-EORI-2")
+    "return 200 OK with NotificationEmail when valid EORI is provided" in new Setup {
+      val eori                     = "GB123456789000"
+      val email: NotificationEmail = NotificationEmail("user@example.com", LocalDateTime.now())
 
-      "when service succeeds" - {
-        "should return 200 OK with JSON body" in {
-          when(mockUserService.getAuthorisedEoris(testEori)).thenReturn(Future.successful(authorisedEoris))
+      when(mockUserService.getNotificationEmail(eori))
+        .thenReturn(Future.successful(email))
 
-          val request  = FakeRequest(GET, s"/authorised-eoris/$testEori")
-          val response = controller.getAuthorisedEoris(testEori).apply(request)
+      val request: FakeRequest[JsObject] = FakeRequest(POST, routes.UserController.getNotificationEmail.url)
+        .withHeaders("Content-Type" -> "application/json")
+        .withBody(Json.obj("eori" -> eori))
 
-          status(response) mustBe OK
-          contentAsJson(response) mustBe Json.toJson(authorisedEoris)
-        }
-      }
+      val result: Future[Result] = route(app, request).value
 
-      "when service fails" - {
-        "should return 500 InternalServerError with error message" in {
-          val exception = new Exception("User not found")
-          when(mockUserService.getAuthorisedEoris(testEori)).thenReturn(Future.failed(exception))
-
-          val request  = FakeRequest(GET, s"/authorised-eoris/$testEori")
-          val response = controller.getAuthorisedEoris(testEori).apply(request)
-
-          status(response) mustBe INTERNAL_SERVER_ERROR
-          contentAsString(response) must include("User not found")
-        }
-      }
+      status(result)        shouldBe OK
+      contentAsJson(result) shouldBe Json.toJson(email)
     }
+
+    "return 400 BadRequest when EORI is missing" in new Setup {
+      val request: FakeRequest[JsObject] = FakeRequest(POST, routes.UserController.getNotificationEmail.url)
+        .withHeaders("Content-Type" -> "application/json")
+        .withBody(Json.obj("invalidField" -> "value"))
+
+      val result: Future[Result] = route(app, request).value
+
+      status(result)        shouldBe BAD_REQUEST
+      contentAsString(result) should include("Missing or invalid 'eori' field")
+    }
+
+    "return 500 InternalServerError when service fails" in new Setup {
+      val eori = "GB123456789000"
+
+      when(mockUserService.getNotificationEmail(eori))
+        .thenReturn(Future.failed(new RuntimeException("Service failure")))
+
+      val request: FakeRequest[JsObject] = FakeRequest(POST, routes.UserController.getNotificationEmail.url)
+        .withHeaders("Content-Type" -> "application/json")
+        .withBody(Json.obj("eori" -> eori))
+
+      val result: Future[Result] = route(app, request).value
+
+      status(result)        shouldBe INTERNAL_SERVER_ERROR
+      contentAsString(result) should include("Service failure")
+    }
+  }
+
+  "UserController.setupUser" should {
+
+    "return 201 Created with user details when valid EORI is provided" in new Setup {
+      val eori                     = "GB123456789000"
+      val userDetails: UserDetails = UserDetails(
+        eori = eori,
+        additionalEmails = Seq.empty,
+        authorisedUsers = Seq.empty,
+        companyInformation = CompanyInformation(), // assuming default constructor exists
+        notificationEmail = NotificationEmail("user@example.com", LocalDateTime.now())
+      )
+
+      when(mockUserService.getOrCreateUser(eori))
+        .thenReturn(Future.successful(userDetails))
+
+      val request: FakeRequest[JsObject] = FakeRequest(GET, routes.UserController.setupUser().url)
+        .withHeaders("Content-Type" -> "application/json")
+        .withBody(Json.obj("eori" -> eori))
+
+      val result: Future[Result] = route(app, request).value
+
+      status(result)        shouldBe CREATED
+      contentAsJson(result) shouldBe Json.toJson(userDetails)
+    }
+
+    "return 400 BadRequest when EORI is missing" in new Setup {
+      val request: FakeRequest[JsObject] = FakeRequest(GET, routes.UserController.setupUser().url)
+        .withHeaders("Content-Type" -> "application/json")
+        .withBody(Json.obj("wrongField" -> "value"))
+
+      val result: Future[Result] = route(app, request).value
+
+      status(result)        shouldBe BAD_REQUEST
+      contentAsString(result) should include("Missing or invalid 'eori' field")
+    }
+  }
+
+  "UserController.getAuthorisedEoris" should {
+
+    "return 200 OK with list of authorised EORIs" in new Setup {
+      val eori                         = "GB123456789000"
+      val authorisedEoris: Seq[String] = Seq("GB111111111111", "GB222222222222")
+
+      when(mockUserService.getAuthorisedEoris(eori))
+        .thenReturn(Future.successful(authorisedEoris))
+
+      val request: FakeRequest[AnyContentAsEmpty.type] =
+        FakeRequest(GET, routes.UserController.getAuthorisedEoris(eori).url)
+
+      val result: Future[Result] = route(app, request).value
+
+      status(result)        shouldBe OK
+      contentAsJson(result) shouldBe JsArray(authorisedEoris.map(JsString(_)))
+    }
+
+    "return 500 InternalServerError when service fails" in new Setup {
+      val eori = "GB123456789000"
+
+      when(mockUserService.getAuthorisedEoris(eori))
+        .thenReturn(Future.failed(new RuntimeException("Service failure")))
+
+      val request: FakeRequest[AnyContentAsEmpty.type] =
+        FakeRequest(GET, routes.UserController.getAuthorisedEoris(eori).url)
+
+      val result: Future[Result] = route(app, request).value
+
+      status(result)        shouldBe INTERNAL_SERVER_ERROR
+      contentAsString(result) should include("Service failure")
+    }
+  }
+
+  trait Setup {
+    val mockUserService: UserService = mock[UserService]
+
+    val app: Application = application
+      .overrides(inject.bind[UserService].toInstance(mockUserService))
+      .build()
   }
 }
