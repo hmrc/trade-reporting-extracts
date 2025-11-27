283acae16d0093a359b1745c409509be5f641e5f
MichaelAAbbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-11-27 09:00:43 +0000

    TRE-970 Fixed key bug swap from refactor

--- app/uk/gov/hmrc/tradereportingextracts/config/EISConfig.scala
+++ app/uk/gov/hmrc/tradereportingextracts/config/EISConfig.scala
@@ -28,8 +28,8 @@ class EISConfig @Inject() (servicesConfig: ServicesConfig, config: Configuration
   val url: String     = baseUrl + context
 
   val authTokenAPI1: String =
-    config.get[String]("eis.auth-token")
-  val authTokenAPI6: String = config.get[String]("microservice.services.eis.auth-token")
+    config.get[String]("microservice.services.eis.auth-token")
+  val authTokenAPI6: String = config.get[String]("eis.auth-token")
 
   val requestTraderReportMaxRetries: Int =
     config.get[Int]("microservice.services.eis.request-trader-report.max-retries")
